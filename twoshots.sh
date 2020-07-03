8e375858bf7240b20a21f164fbe9cbc60646206c
Bogdan Shchogolev <bogdan993000@gmail.com>
2020-07-03 22:06:21 +0300

    Implement args parsing

--- twoshots.sh
+++ twoshots.sh
@@ -1,13 +1,30 @@
 #!/bin/sh
 
-TIMEOUT=30
+function usage()
+{
+	echo "Usage: $(basename $0) [options]"
+	echo -e "-t <time>      \tScan timeout in seconds"
+	echo -e "-i <interface> \tInterface name"
+	echo -e "-h             \tShow this help"
+	exit
+}
 
-if [ "$1" ]
-then
-	IFACE=$1
-else
-	IFACE="wlan0"
-fi
+TIMEOUT=30
+IFACE="wlan0"
+
+while getopts "t:i:h" opt
+do
+	case $opt in
+		t)
+			TIMEOUT=${OPTARG};;
+		i)
+			IFACE=${OPTARG};;
+		h)
+			usage;;
+		*)
+			usage;;
+	esac
+done
 
 echo "[*] Scanning networks around..."
 
